f3ff9ca6d5ae5bf9167e6cf29821dda8f34847e5
황상현 <aa112233bb12ik@gmail.com>
2025-06-18 11:59:29 +0900

    3333333

--- simulator/urls.py
+++ simulator/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path, include
 from django.contrib.auth import views as auth_views
+from django.contrib.auth.views import LogoutView
 from . import views
+
 
 app_name = 'Tradium'
 
@@ -15,7 +17,8 @@ urlpatterns = [
     path('api/stock/<int:stock_id>/history/', views.get_stock_history, name='get_stock_history'),
     path('ranking/', views.ranking_view, name='ranking'),
     path("login/", auth_views.LoginView.as_view(template_name='simulator/login.html'), name="login"),
-    path("logout/", auth_views.LogoutView.as_view(), name="logout"),
+    path('logout/', LogoutView.as_view(next_page='Tradium:login'), name='logout'),
     path('signup/', views.signup, name='signup'),
+    
 
 ]
